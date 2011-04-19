c288f5713d28d6dd4ddb5fba0f96476360873b79
unknown <Vu Tran@.(none)>
2011-04-19 15:39:32 -0700

    messing around

--- BVrama/BVrama.cpp
+++ BVrama/BVrama.cpp
@@ -6,19 +6,31 @@
 #include <cxcore.h>
 #include <highgui.h>
 
+using namespace cv;
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 		//Cloned and Updated - Ben
 		// Open the file.
         IplImage *img = cvLoadImage("photo.jpg");
+
         if (!img) {
                 printf("Error: Couldn't open the image file.\n");
                 return 1;
         }
 
+		Mat src = Mat(img,true);
+		Mat dst;
+		bilateralFilter( src, dst, 15, 100.0, 100.0,BORDER_DEFAULT);
+
+		imwrite("photo1.jpg",dst);
+
         // Display the image.
         cvNamedWindow("Image:", CV_WINDOW_AUTOSIZE);
         cvShowImage("Image:", img);
+		cvNamedWindow("mat", 1);
+		imshow("mat", dst);
+
 
         // Wait for the user to press a key in the GUI window.
         cvWaitKey(0);
